928c085cfecc1a1644c03b75673e5863ff07b019
tetsuto-k <tkatsura@ucsb.edu>
2023-12-21 08:58:23 -0800

    Project popup

---
+++ assets/js/popupscript.js
@@ -0,0 +1,22 @@
+// popupscript.js
+
+function showPopup(htmlFileName) {
+    // Get the popup container
+    const popupContainer = document.getElementById("popupContainer");
+
+    // Load content from the external HTML file
+    fetch(htmlFileName)
+        .then(response => response.text())
+        .then(content => {
+            // Set the content inside the popup container
+            popupContainer.innerHTML = content;
+
+            // Show the popup
+            popupContainer.classList.add("active");
+        });
+}
+
+function hidePopup() {
+    // Hide the popup
+    document.getElementById("popupContainer").classList.remove("active");
+}
--- pages/projects.md
+++ pages/projects.md
@@ -8,3 +8,4 @@ header:
    image_fullwidth  : "header_homepage_13.jpg"
 permalink           : "/projects/"
 ---
+
